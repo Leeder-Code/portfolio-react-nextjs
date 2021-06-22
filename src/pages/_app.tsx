78c35bf5b1c6213ada802fa8ba4947c5b99e44c0
Mati-Code <mati.leeder@gmail.com>
2021-06-22 14:29:57 +0200

    add global styles & themeprovider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,34 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const theme = {}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0px;
+    padding: 0px;
+  }
+  body{
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  button {
+    cursor: pointer;
+  }
+  button:hover {
+    opacity: 0.8;
+  }
+`
