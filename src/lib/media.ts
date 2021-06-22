670b141592e7d90391d11617faed2e23898b6157
Mati-Code <mati.leeder@gmail.com>
2021-06-22 13:56:36 +0200

    add styled-media-query

---
+++ src/lib/media.ts
@@ -0,0 +1,10 @@
+import { generateMedia } from 'styled-media-query'
+
+export const media = generateMedia({
+  mobile: '417px',
+  sm: '640px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1280px',
+  '2xl': '1536px',
+})
