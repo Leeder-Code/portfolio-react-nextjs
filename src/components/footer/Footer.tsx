4db1edc5df7ba07491db99f5b52c49edc1263885
Mati-Code <mati.leeder@gmail.com>
2021-08-06 01:52:49 +0200

    change opacity in title text & add footer template

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,6 +6,9 @@ const Footer: FC = () => {
   return <FooterStyles></FooterStyles>
 }
 
-const FooterStyles = styled.footer``
+const FooterStyles = styled.footer`
+  padding: 20px 0px;
+  border-top: 1px solid #ffffff10;
+`
 
 export default Footer
--- src/components/layout/SectionTitleText.tsx
+++ src/components/layout/SectionTitleText.tsx
@@ -3,8 +3,9 @@ import styled from 'styled-components'
 const TitleStyles = styled.h1`
   font-size: 6rem;
   color: transparent;
-  -webkit-text-stroke: 1.3px whitesmoke;
+  -webkit-text-stroke: 1.3px #ffffff;
   padding: 50px 0px;
+  opacity: 0.6;
 `
 
 export default TitleStyles
