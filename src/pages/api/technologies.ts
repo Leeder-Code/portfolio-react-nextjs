8c9a69ee7fc55ea0d08043254397507211760a93
Mati-Code <mati.leeder@gmail.com>
2021-08-06 00:18:06 +0200

    check

--- src/pages/api/technologies.ts
+++ src/pages/api/technologies.ts
@@ -1,7 +1,7 @@
 import { NextApiHandler } from 'next'
 
 const handler: NextApiHandler = async (req, res) => {
-  res.status(200).json({ technology: 'ok' })
+  res.status(200).json([{ technology: 'ok' }])
 }
 
 export default handler
